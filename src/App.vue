02bea39cfb307f27e9e901b11926409b0b4a9e31
Richard Bodsworth <richardbodsworth@gmail.com>
2021-01-14 09:11:53 +0000

    MVP done

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+      <countries-list :countries='countries'></countries-list>
+      <country-detail :country='selectedCountry'></country-detail>
+    
+    </div>
+  </div>
+</template>
+
+<script>
+  import { eventBus } from './main.js'
+  import CountriesList from './components/CountriesList.vue';
+  import CountriesDetail from './components/CountriesDetail.vue';
+
+export default {
+   name: 'app',
+   data(){
+     return {
+       countries: [],
+       selectedCountry: null
+     };
+   },
+   mounted(){
+     fetch("https://restcountries.eu/rest/v2/all")
+     .then( res => res.json())
+     .then(countries => this.countries = countries)
+
+     eventBus.$on('country-selected', (country) => {
+       this.selectedCountry = country;
+   })
+     },
+    components: {
+      "countries-list": CountriesList,
+      "country-detail": CountriesDetail
+    }
+}
+</script>
+
+<style>
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/components/CountriesDetail.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+    <div v-if='country'>
+        <h3>{{country.name}}</h3>
+        <p>Name: {{country.name}}</p>
+        <p>Capital: {{country.capital}}</p>
+        <p>Population: {{country.population}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "country-detail", 
+    props: ['country']
+}
+</script>
+<style lang="css" scoped>
+
+</style>
